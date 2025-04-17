12ed94685642d92e741aaa63e7cb0f9c29127c81
alekseevavika2009 <alekseevavika2009@yandex.ru>
2025-04-17 13:08:03 +0300

    commit new main and fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.random.RandomGenerator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("Number = " + calc (i, RandomGenerator.getDefault().nextInt()));
         }
+    }
+
+    static int calc(int a, int b) {
+    if (a > b) {
+        return a * b;
+    }
+    else if (b >0) {
+        return a / b;
+    }
+    return 0;
     }
 }
